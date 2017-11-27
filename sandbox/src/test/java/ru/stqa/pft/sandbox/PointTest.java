82bc0261c2086c47ea3eba748df99fbbb9f8346e
NepVik <ivikky1234@gmail.com>
2017-11-27 21:34:50 +0300

    Добавили еще 1 тест в класс PointTest

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -11,5 +11,10 @@ public class PointTest {
     Point p2 = new Point(1, 5);
 
     Assert.assertEquals(p1.distance(p2), 15.0);
+
+    Point p3 = new Point(-2.3, 4);
+    Point p4 = new Point(8.5, 0.7);
+
+    Assert.assertEquals(p3.distance(p4), 11.292918134831227);
   }
 }
