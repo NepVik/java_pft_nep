cbda86b8d5e6487c95c9c0c26e290a02a9710d85
NepVik <ivikky1234@gmail.com>
2017-11-27 21:11:56 +0300

    Внесены изменения в классы Point и Task2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -4,14 +4,9 @@ public class Task2 {
 
   public static void main (String args[]) {
 
-    Point d1 = new Point(3, 2, 7, 5);
-    Point d2 = new Point(5, 4, 1, 9);
+    Point p1 = new Point(1, 20);
+    Point p2 = new Point(1, 5);
 
-    System.out.println("1. Расстояние от точки c координатами: \nx1 = " + d1.x1 + "\ny1 = " + d1.y1 + "\n до точки с координатами: \nx2 = " + d1.x2 + "\ny2 = " + d1.y2 + "\nравно - " + d1.distance());
-    System.out.println();
-    System.out.println("2. Расстояние от точки c координатами: \nx1 = " + d2.x1 + "\ny1 = " + d2.y1 + "\n до точки с координатами: \nx2 = " + d2.x2 + "\ny2 = " + d2.y2 + "\nравно - " + d2.distance());
-
-
+    System.out.println("Расстояние от точки p1 до точки p2 равно - " + p1.distance(p2));
   }
-
 }
