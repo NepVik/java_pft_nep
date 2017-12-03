1a24d44296835f2f44690b813a7d367ed1ee183e
NepVik <ivikky1234@gmail.com>
2017-12-03 13:29:26 +0300

    Замена наследования на делегирование, созданадвухуровневая архитектура

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook;
 
-public class CroupData {
+public class GroupData {
   private final String name;
   private final String header;
   private final String footer;
 
-  public CroupData(String name, String header, String footer) {
+  public GroupData(String name, String header, String footer) {
     this.name = name;
     this.header = header;
     this.footer = footer;
