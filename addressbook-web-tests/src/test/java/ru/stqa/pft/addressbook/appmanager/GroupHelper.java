3393fad7bee2cac2bd38c6d7a68b2dfd411a1d0e
NepVik <ivikky1234@gmail.com>
2017-12-25 22:37:05 +0300

    Revert "Улучшение внешнего вида проверок для тестов групп"
    This reverts commit f78e7c03f03c1620c415de2c697380d2b0901087.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.HashSet;
 import java.util.List;
@@ -81,8 +80,8 @@ public class GroupHelper extends HelperBase{
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public Groups all() {
-    Groups groups = new Groups() ;
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>() ;
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
