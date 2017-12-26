f05ae97a6b736c7a24efb0197f3f4110dd309309
NepVik <ivikky1234@gmail.com>
2017-12-26 22:27:22 +0300

    Кэширование в тестах для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -11,13 +11,14 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
-    app.group().create(group);
-    Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      GroupData group = new GroupData().withName("test2");
+      app.group().create(group);
+      Groups after = app.group().all();
+      assertThat(after.size(),equalTo(before.size() + 1));
+      assertThat(after,equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
   }
 }
